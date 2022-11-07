282aec7a6ce158d62ccdde854ea83bd2c9e2d2e0
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-11-07 23:33:35 +0600

    basic stracture setup

---
+++ src/component/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link to="/"> About</Link>
+        <Link to="/"> Services</Link>
+        <Link to="/"> Portfolio</Link>
+        <Link to="/"> Contact Us</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="container mx-auto py-3">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <Link to="/" className="">
+            <img
+              src="https://i.ibb.co/fXWkfQL/CAPTURE-MOMENT.png"
+              className="h-10"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className="btn m-1">
+              User Name
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link>User Profile</Link>
+              </li>
+              <li>
+                <Link>Log out</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
